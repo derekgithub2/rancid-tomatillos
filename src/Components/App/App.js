dd3ca2e16b053a10b4c7bf6e7c6b6d71ea9ee832
Matt Rowan <matt.rowan121@gmail.com>
2023-02-02 21:14:38 -0500

    Add component did mount & will render movies func

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 import Nav from '../Nav/Nav'
+import movieData from '../../movieData'
+import Movies from '../Movies/Movies'
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      movies: [],
+    };
   }
 
+  componentDidMount() {
+    this.setState({movies: movieData.movies})
+  }
   // functions in here
 
   render() {
     return(
       <main className='app'>
         <Nav />
+        <Movies movies={this.state.movies}/>
       </main>
     )
   }
