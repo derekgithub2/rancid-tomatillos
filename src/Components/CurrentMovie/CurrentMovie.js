ce46c1d2b0f97743c7d2397798ede3cee6478b10
Matt Rowan <matt.rowan121@gmail.com>
2023-02-04 14:11:55 -0500

    Add formatter func & display additional properties

--- src/Components/CurrentMovie/CurrentMovie.js
+++ src/Components/CurrentMovie/CurrentMovie.js
@@ -11,13 +11,24 @@ const CurrentMovie = ({ currentMovie, displayAllMovies }) => {
         backgroundRepeat: 'no-repeat',
     }
 
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    });
+
     return (
         <div className="current-movie" style={currentMovieStyle}>
             <section className="left-section">
                 <img className="back-button" src={backbutton} alt="backbutton" onClick={() => displayAllMovies()}/>
+                <p>Runtime: {currentMovie.runtime} min.</p>
+                <p>Genre: {currentMovie.genres.join(", ")}</p>
+                <p>Release Date: {new Date(currentMovie.release_date.split("-").join("/")).toLocaleDateString('en-us', {year:"numeric", month:"short", day:"numeric"}) }</p>
                 <h3>{currentMovie.title}</h3>
                 <p>Average Rating: {currentMovie.average_rating.toFixed(2)}</p>
-                <p>Release Date: {currentMovie.release_date}</p>
+                <p>Tagline: {currentMovie.tagline}</p>
+                <p>Overview: {currentMovie.overview}</p>
+                <p>Budget: {formatter.format(currentMovie.budget)}</p>
+                <p>Revenue: {formatter.format(currentMovie.revenue)}</p>
             </section>
         </div>
     )
