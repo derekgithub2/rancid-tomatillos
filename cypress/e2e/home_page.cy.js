5300d791d87d20268f58f7793b851d299cc3bbad
Derek Yeh <derek.yeh91@gmail.com>
2023-02-07 08:29:24 -0800

    Add baseUR

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -1,10 +1,11 @@
 describe('Home page', () => {
-    beforeEach(() => {
-        cy.visit('/')
-        // can add '/ideas' if thats the url extension 
-    })
+    // beforeEach(() => {
+    //     cy.visit('/')
+    //     // can add '/ideas' if thats the url extension 
+    // })
 
     it('should display a navigation bar and a grid of movies', () => {
+        cy.visit('/')
         cy.get('nav')
     })
 })
