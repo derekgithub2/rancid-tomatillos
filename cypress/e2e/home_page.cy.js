ef5653d94993f7ef49f494ee3f5963a7dd568a41
Derek Yeh <derek.yeh91@gmail.com>
2023-02-07 08:34:23 -0800

    Add test for genres

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -1,11 +1,17 @@
 describe('Home page', () => {
-    // beforeEach(() => {
-    //     cy.visit('/')
-    //     // can add '/ideas' if thats the url extension 
-    // })
-
-    it('should display a navigation bar and a grid of movies', () => {
+    beforeEach(() => {
         cy.visit('/')
-        cy.get('nav')
+        // can add '/ideas' if thats the url extension 
+    })
+
+    it('should display a navigation bar with a list of genres', () => {
+        // cy.visit('/')
+        cy.get('ul')
+          .contains('Action')
+          .contains('Comedy')
+          .contains('Drama')
+          .contains('Family')
+          .contains('Horror')
+          .contains('Thriller')
     })
 })
